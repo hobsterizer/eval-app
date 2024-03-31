63689072b5ec5308a6e02eac03896f4f6491ca17
Ahmed Ehab <ahmedehab25299@gmail.com>
2024-03-31 21:05:51 +0200

    autologin

--- app/Login.js
+++ app/Login.js
@@ -5,130 +5,132 @@ import { useRouter } from "next/navigation";
 import { UserIcon, LockClosedIcon } from "@heroicons/react/solid";
 import { ReactComponentElement as NextSvg } from "../public/next.svg";
 function Login() {
-    const router = useRouter();
-    const [formIsValid, setFormIsValid] = useState(false);
-    const [email, setEmail] = useState({
-        value: "admin@mail.com",
-        isValid: null,
+  const router = useRouter();
+  const [formIsValid, setFormIsValid] = useState(false);
+  const [email, setEmail] = useState({
+    value: "admin@mail.com",
+    isValid: null,
+  });
+  const [password, setPassword] = useState({
+    value: "admin1234",
+    isValid: null,
+  });
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      setFormIsValid(email.isValid && password.isValid);
+    }, 500);
+    return () => {
+      clearTimeout(identifier);
+    };
+  }, [email.isValid, password.isValid]);
+
+  const emailChangeHandler = (event) => {
+    setEmail({
+      value: event.target.value,
+      isValid: RegExp(
+        /(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gi
+      ).test(event.target.value),
     });
-    const [password, setPassword] = useState({
-        value: "admin1234",
-        isValid: null,
+  };
+
+  const passwordChangeHandler = (event) => {
+    setPassword({
+      value: event.target.value,
+      isValid: event.target.value.trim().length >= 8,
     });
+  };
 
-    useEffect(() => {
-        const identifier = setTimeout(() => {
-            setFormIsValid(email.isValid && password.isValid);
-        }, 500);
-        return () => {
-            clearTimeout(identifier);
-        };
-    }, [email.isValid, password.isValid]);
+  const submitHandler = (event) => {
+    event.preventDefault();
+    router.replace("/products");
+  };
+  useEffect(() => {
+    submitHandler();
+  }, []);
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <Card
+        className="max-w-sm mx-auto"
+        decoration="bottom"
+        decorationColor="indigo"
+      >
+        <form className="flex flex-col" onSubmit={submitHandler}>
+          <svg
+            className="m-10 justify-self-center self-center"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 394 80"
+            width="200"
+          >
+            <path
+              fill="white"
+              d="M262 0h68.5v12.7h-27.2v66.6h-13.6V12.7H262V0ZM149 0v12.7H94v20.4h44.3v12.6H94v21h55v12.6H80.5V0h68.7zm34.3 0h-17.8l63.8 79.4h17.9l-32-39.7 32-39.6h-17.9l-23 28.6-23-28.6zm18.3 56.7-9-11-27.1 33.7h17.8l18.3-22.7z"
+            />
+            <path
+              fill="white"
+              d="M81 79.3 17 0H0v79.3h13.6V17l50.2 62.3H81Zm252.6-.4c-1 0-1.8-.4-2.5-1s-1.1-1.6-1.1-2.6.3-1.8 1-2.5 1.6-1 2.6-1 1.8.3 2.5 1a3.4 3.4 0 0 1 .6 4.3 3.7 3.7 0 0 1-3 1.8zm23.2-33.5h6v23.3c0 2.1-.4 4-1.3 5.5a9.1 9.1 0 0 1-3.8 3.5c-1.6.8-3.5 1.3-5.7 1.3-2 0-3.7-.4-5.3-1s-2.8-1.8-3.7-3.2c-.9-1.3-1.4-3-1.4-5h6c.1.8.3 1.6.7 2.2s1 1.2 1.6 1.5c.7.4 1.5.5 2.4.5 1 0 1.8-.2 2.4-.6a4 4 0 0 0 1.6-1.8c.3-.8.5-1.8.5-3V45.5zm30.9 9.1a4.4 4.4 0 0 0-2-3.3 7.5 7.5 0 0 0-4.3-1.1c-1.3 0-2.4.2-3.3.5-.9.4-1.6 1-2 1.6a3.5 3.5 0 0 0-.3 4c.3.5.7.9 1.3 1.2l1.8 1 2 .5 3.2.8c1.3.3 2.5.7 3.7 1.2a13 13 0 0 1 3.2 1.8 8.1 8.1 0 0 1 3 6.5c0 2-.5 3.7-1.5 5.1a10 10 0 0 1-4.4 3.5c-1.8.8-4.1 1.2-6.8 1.2-2.6 0-4.9-.4-6.8-1.2-2-.8-3.4-2-4.5-3.5a10 10 0 0 1-1.7-5.6h6a5 5 0 0 0 3.5 4.6c1 .4 2.2.6 3.4.6 1.3 0 2.5-.2 3.5-.6 1-.4 1.8-1 2.4-1.7a4 4 0 0 0 .8-2.4c0-.9-.2-1.6-.7-2.2a11 11 0 0 0-2.1-1.4l-3.2-1-3.8-1c-2.8-.7-5-1.7-6.6-3.2a7.2 7.2 0 0 1-2.4-5.7 8 8 0 0 1 1.7-5 10 10 0 0 1 4.3-3.5c2-.8 4-1.2 6.4-1.2 2.3 0 4.4.4 6.2 1.2 1.8.8 3.2 2 4.3 3.4 1 1.4 1.5 3 1.5 5h-5.8z"
+            />
+          </svg>
+          <Metric className="self-center">Login</Metric>
+          {email.isValid !== false ? (
+            <TextInput
+              icon={UserIcon}
+              className="mt-5"
+              type="text"
+              id="email"
+              value={email.value}
+              placeholder="Username..."
+              onChange={emailChangeHandler}
+            />
+          ) : (
+            <TextInput
+              icon={UserIcon}
+              className="mt-5"
+              type="text"
+              id="email"
+              value={email.value}
+              placeholder="Username..."
+              onChange={emailChangeHandler}
+              error={true}
+              errorMessage="invalid email"
+            />
+          )}
+          {password.isValid !== false ? (
+            <TextInput
+              icon={LockClosedIcon}
+              className="mt-5"
+              type="password"
+              id="password"
+              value={password.value}
+              placeholder="Password..."
+              onChange={passwordChangeHandler}
+            />
+          ) : (
+            <TextInput
+              icon={LockClosedIcon}
+              className="mt-5"
+              type="password"
+              id="password"
+              value={password.value}
+              placeholder="Password..."
+              onChange={passwordChangeHandler}
+              error={true}
+              errorMessage="password must be at least 8 characters"
+            />
+          )}
 
-    const emailChangeHandler = (event) => {
-        setEmail({
-            value: event.target.value,
-            isValid: RegExp(
-                /(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gi
-            ).test(event.target.value),
-        });
-    };
-
-    const passwordChangeHandler = (event) => {
-        setPassword({
-            value: event.target.value,
-            isValid: event.target.value.trim().length >= 8,
-        });
-    };
-
-    const submitHandler = (event) => {
-        event.preventDefault();
-        router.replace("/products");
-    };
-    return (
-        <div className="flex min-h-screen flex-col items-center justify-center">
-            <Card
-                className="max-w-sm mx-auto"
-                decoration="bottom"
-                decorationColor="indigo"
-            >
-                <form className="flex flex-col" onSubmit={submitHandler}>
-                    <svg
-                        className="m-10 justify-self-center self-center"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 394 80"
-                        width="200"
-                    >
-                        <path
-                            fill="white"
-                            d="M262 0h68.5v12.7h-27.2v66.6h-13.6V12.7H262V0ZM149 0v12.7H94v20.4h44.3v12.6H94v21h55v12.6H80.5V0h68.7zm34.3 0h-17.8l63.8 79.4h17.9l-32-39.7 32-39.6h-17.9l-23 28.6-23-28.6zm18.3 56.7-9-11-27.1 33.7h17.8l18.3-22.7z"
-                        />
-                        <path
-                            fill="white"
-                            d="M81 79.3 17 0H0v79.3h13.6V17l50.2 62.3H81Zm252.6-.4c-1 0-1.8-.4-2.5-1s-1.1-1.6-1.1-2.6.3-1.8 1-2.5 1.6-1 2.6-1 1.8.3 2.5 1a3.4 3.4 0 0 1 .6 4.3 3.7 3.7 0 0 1-3 1.8zm23.2-33.5h6v23.3c0 2.1-.4 4-1.3 5.5a9.1 9.1 0 0 1-3.8 3.5c-1.6.8-3.5 1.3-5.7 1.3-2 0-3.7-.4-5.3-1s-2.8-1.8-3.7-3.2c-.9-1.3-1.4-3-1.4-5h6c.1.8.3 1.6.7 2.2s1 1.2 1.6 1.5c.7.4 1.5.5 2.4.5 1 0 1.8-.2 2.4-.6a4 4 0 0 0 1.6-1.8c.3-.8.5-1.8.5-3V45.5zm30.9 9.1a4.4 4.4 0 0 0-2-3.3 7.5 7.5 0 0 0-4.3-1.1c-1.3 0-2.4.2-3.3.5-.9.4-1.6 1-2 1.6a3.5 3.5 0 0 0-.3 4c.3.5.7.9 1.3 1.2l1.8 1 2 .5 3.2.8c1.3.3 2.5.7 3.7 1.2a13 13 0 0 1 3.2 1.8 8.1 8.1 0 0 1 3 6.5c0 2-.5 3.7-1.5 5.1a10 10 0 0 1-4.4 3.5c-1.8.8-4.1 1.2-6.8 1.2-2.6 0-4.9-.4-6.8-1.2-2-.8-3.4-2-4.5-3.5a10 10 0 0 1-1.7-5.6h6a5 5 0 0 0 3.5 4.6c1 .4 2.2.6 3.4.6 1.3 0 2.5-.2 3.5-.6 1-.4 1.8-1 2.4-1.7a4 4 0 0 0 .8-2.4c0-.9-.2-1.6-.7-2.2a11 11 0 0 0-2.1-1.4l-3.2-1-3.8-1c-2.8-.7-5-1.7-6.6-3.2a7.2 7.2 0 0 1-2.4-5.7 8 8 0 0 1 1.7-5 10 10 0 0 1 4.3-3.5c2-.8 4-1.2 6.4-1.2 2.3 0 4.4.4 6.2 1.2 1.8.8 3.2 2 4.3 3.4 1 1.4 1.5 3 1.5 5h-5.8z"
-                        />
-                    </svg>
-                    <Metric className="self-center">Login</Metric>
-                    {email.isValid !== false ? (
-                        <TextInput
-                            icon={UserIcon}
-                            className="mt-5"
-                            type="text"
-                            id="email"
-                            value={email.value}
-                            placeholder="Username..."
-                            onChange={emailChangeHandler}
-                        />
-                    ) : (
-                        <TextInput
-                            icon={UserIcon}
-                            className="mt-5"
-                            type="text"
-                            id="email"
-                            value={email.value}
-                            placeholder="Username..."
-                            onChange={emailChangeHandler}
-                            error={true}
-                            errorMessage="invalid email"
-                        />
-                    )}
-                    {password.isValid !== false ? (
-                        <TextInput
-                            icon={LockClosedIcon}
-                            className="mt-5"
-                            type="password"
-                            id="password"
-                            value={password.value}
-                            placeholder="Password..."
-                            onChange={passwordChangeHandler}
-                        />
-                    ) : (
-                        <TextInput
-                            icon={LockClosedIcon}
-                            className="mt-5"
-                            type="password"
-                            id="password"
-                            value={password.value}
-                            placeholder="Password..."
-                            onChange={passwordChangeHandler}
-                            error={true}
-                            errorMessage="password must be at least 8 characters"
-                        />
-                    )}
-
-                    <Button
-                        className="w-1/4 self-center mt-5"
-                        color="indigo"
-                        size="lg"
-                        
-                        type="submit"
-                    >
-                        Login
-                    </Button>
-                </form>
-            </Card>
-        </div>
-    );
+          <Button
+            className="w-1/4 self-center mt-5"
+            color="indigo"
+            size="lg"
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
+      </Card>
+    </div>
+  );
 }
 export default Login;
